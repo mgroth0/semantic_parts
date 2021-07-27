5eaf24eed1f140958a85d388c1672cebae12b4df
Matt Groth <mgroth49@gmail.com>
2021-07-27 03:27:11 -0400

    idk

--- preprocess/auto_crop.py
+++ preprocess/auto_crop.py
@@ -3,14 +3,20 @@ import os
 from PIL import Image
 import numpy as np
 
-DEBUG = 'Face/Ba_Jan2018_F_Face_FU3.png'
+DEBUG = None
+#DEBUG = 'Face/Ba_Jan2018_F_Face_FU3.png'
+#DEBUG = 'House/Aa_Blu0500_M_House_CON.png'
 
 data_folder = '../data/Real Object Drawings/data/'
 raw_folder = data_folder + 'raw/'
 crop_demo_folder = data_folder + 'crop_demo/'
 os.makedirs(crop_demo_folder, exist_ok=True)
 
+# THRESHOLD = 255
+THRESHOLD = 100
+
 for category in os.listdir(raw_folder):
+    if category == '.DS_Store': continue
     cat_folder = raw_folder + category + '/'
     for im_file in os.listdir(cat_folder):
         if im_file == 'Labeled': continue
@@ -38,7 +44,7 @@ for category in os.listdir(raw_folder):
 
         for y in range(im.shape[0]):
             row = im[y]
-            colored = any(any(c != 255 for c in px) for px in row)
+            colored = any(any(c < THRESHOLD for c in px) for px in row)
             if colored:
                 last_colored_y = y
                 if first_colored_y is None:
@@ -46,7 +52,7 @@ for category in os.listdir(raw_folder):
 
         for x in range(im.shape[1]):
             col = im[:, x]
-            colored = any(any(c != 255 for c in px) for px in col)
+            colored = any(any(c < THRESHOLD for c in px) for px in col)
             if colored:
                 last_colored_x = x
                 if first_colored_x is None:
